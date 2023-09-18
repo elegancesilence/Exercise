65ef86d69cf5dfb573224705c9baac9d39372456
Elegance <1785803241@qq.com>
2023-09-18 15:43:28 +0800

    Learn the BFS

--- Algorithm_Exercise/AcWing/AcWing_846.cpp
+++ Algorithm_Exercise/AcWing/AcWing_846.cpp
@@ -1,67 +1,62 @@
-#include <iostream>
-#include <cstring>
-#include <algorithm>
-#include <cmath>
+#include <bits/stdc++.h>
 
-using namespace std;
+#define int long long
 
-constexpr int N = 1e5 + 5, M = N * 2;
+using LL = long long;
+using PII = std::pair<int, int>;
 
-int n;
-int idx;
-int h[N], val[M], ne[M];
-bool st[N];
+const int MOD = 1e9 + 7;
+const int N = 1e2 + 5;
 
-int ans = N;
+int n, m;
+int g[N][N], d[N][N];
 
-void add(int x, int y)
-{
-    val[idx] = y;
+int bfs() {
+  std::queue<PII> q;
+  q.push({0, 0});
 
-    ne[idx] = h[x];
-    h[x] = idx++;
+  d[0][0] = 0;
+
+  int dx[4] = {1, 0, -1, 0};
+  int dy[4] = {0, 1, 0, -1};
+  while (!q.empty()) {
+    PII t = q.front();
+    q.pop();
+
+    for (int i = 0; i < 4; ++i) {
+      int x = t.first + dx[i];
+      int y = t.second + dy[i];
+      if (x >= 0 && x < n && y >= 0 && y < m && g[x][y] == 0 && d[x][y] == -1) {
+        d[x][y] = d[t.first][t.second] + 1;
+
+        q.push({x, y});
+      }
+    }
+  }
+
+  return d[n - 1][m - 1];
 }
 
-int dfs(int u)
-{
-    st[u] = true;
+void solution() {
+  std::cin >> n >> m;
 
-    int sum = 1, res = 0;
-    for (int i = h[u]; i != -1; i = ne[i])
-    {
-        int t = val[i];
-        if (!st[t])
-        {
-            int s = dfs(t);
-
-            res = max(res, s);
-            sum += s;
-        }
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < m; ++j) {
+      std::cin >> g[i][j];
     }
+  }
 
-    res = max(res, n - sum);
-    ans = min(ans, res);
+  memset(d, -1, sizeof d);
 
-    return sum;
+  std::cout << bfs() << std::endl;
 }
 
-int main()
-{
-    cin >> n;
+signed main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
 
-    memset(h, -1, sizeof h);
+  solution();
 
-    int a, b;
-    for (int i = 0; i < n - 1; ++i)
-    {
-        cin >> a >> b;
-
-        add(a, b), add(b, a);
-    }
-
-    dfs(1);
-
-    cout << ans << endl;
-
-    return 0;
+  return 0;
 }
