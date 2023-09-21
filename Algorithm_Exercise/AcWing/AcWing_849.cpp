b03d522c790d6f0d0525ec13d7488c54da9c7877
Elegance <1785803241@qq.com>
2023-09-21 17:11:33 +0800

    Daily exercise

--- Algorithm_Exercise/AcWing/AcWing_849.cpp
+++ Algorithm_Exercise/AcWing/AcWing_849.cpp
@@ -1,54 +1,68 @@
-#include <iostream>
-#include <cstring>
-#include <algorithm>
+#include <bits/stdc++.h>
 
-using namespace std;
+#define int long long
 
+using LL = long long;
+
+const int MOD = 1e9 + 7;
 const int N = 5e2 + 5;
 
 int n, m;
 int dis[N];
-int g[N][N];
 bool st[N];
+std::vector<std::vector<int>> g(N + 1, std::vector<int>(N + 1));
 
-int Dijkstra()
-{
-    memset(dis, 0x3f, sizeof dis);
-    dis[1] = 0;
+void dijkstra() {
+  memset(dis, 0x3f, sizeof dis);
+  dis[1] = 0;
 
-    for (int i = 0; i < n; ++i)
-    {
-        int t = -1;
-        for (int j = 1; j <= n; ++j)
-            if (!st[j] && (t == -1 || dis[t] > dis[j]))
-                t = j;
-
-        st[t] = true;
-
-        for (int j = 1; j <= n; ++j)
-            dis[j] = min(dis[j], dis[t] + g[t][j]);
+  for (int i = 0; i < n; ++i) {
+    int t = -1;
+    for (int j = 1; j <= n; ++j) {
+      if (!st[j] && (t == -1 || dis[j] < dis[t])) {
+        t = j;
+      }
     }
 
-    if (dis[n] == 0x3f3f3f3f)
-        return -1;
-    return dis[n];
+    st[t] = true;
+
+    for (int j = 1; j <= n; ++j) {
+      dis[j] = std::min(dis[j], dis[t] + g[t][j]);
+    }
+  }
+
+  if (dis[n] == 0x3f3f3f3f) {
+    std::cout << -1 << std::endl;
+  } else {
+    std::cout << dis[n] << std::endl;
+  }
 }
 
-int main()
-{
-    cin >> n >> m;
+void solution() {
+  std::cin >> n >> m;
 
-    memset(g, 0x3f, sizeof g);
-
-    int x, y, z;
-    while (m--)
-    {
-        cin >> x >> y >> z;
-
-        g[x][y] = min(g[x][y], z);
+  for (int i = 1; i <= n; ++i) {
+    for (int j = 1; j <= n; ++j) {
+      g[i][j] = 0x3f3f3f3f;
     }
+  }
 
-    cout << Dijkstra() << endl;
+  while (m--) {
+    int a, b, w;
+    std::cin >> a >> b >> w;
 
-    return 0;
+    g[a][b] = std::min(g[a][b], w);
+  }
+
+  dijkstra();
+}
+
+signed main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+
+  solution();
+
+  return 0;
 }
