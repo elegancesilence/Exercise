3d1c3b8ceb7e00b8fe6c7247942edc4e474ecab1
Elegance <1785803241@qq.com>
2023-09-27 15:57:07 +0800

    Daily exercise

--- Algorithm_Exercise/OJ/USST/P482.cpp
+++ Algorithm_Exercise/OJ/USST/P482.cpp
@@ -6,10 +6,21 @@ using LL = long long;
 
 const int MOD = 1e9 + 7;
 
-
+int x, y;
 
 void solution() {
+  int ans = 0;
+  int max = -1;
+  for (int i = 0; i < 7; ++i) {
+    std::cin >> x >> y;
 
+    if (x + y > 8 && x + y > max) {
+      ans = i + 1;
+      max = x + y;
+    }
+  }
+
+  std::cout << ans << std::endl;
 }
 
 signed main() {
