5cee67470299e6e7dd10d625233db764776345ee
Elegance <1785803241@qq.com>
2023-10-16 21:08:02 +0800

    Update Template

--- Algorithm_Exercise/Template.cpp
+++ Algorithm_Exercise/Template.cpp
@@ -6,16 +6,19 @@ using LL = long long;
 
 const int MOD = 1e9 + 7;
 
-void solution() {
-
-}
+void solution() {}
 
 signed main() {
   std::ios::sync_with_stdio(false);
   std::cin.tie(nullptr);
   std::cout.tie(nullptr);
 
-  solution();
+  int t = 1;
+  // std::cin >> t;
+
+  while (t--) {
+    solution();
+  }
 
   return 0;
 }
