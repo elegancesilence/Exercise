4405c1ccc1aa173cd96669d259ada692a6a5b5a4
Elegance <1785803241@qq.com>
2023-10-23 21:37:59 +0800

    Learn greedy

---
+++ Algorithm_Exercise/AcWing/AcWing_104.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+#define int long long
+
+using LL = long long;
+
+const int MOD = 1e9 + 7;
+const int N = 1e5 + 5;
+
+int n;
+int arr[N];
+
+void solution() {
+  std::cin >> n;
+
+  for (int i = 1; i <= n; ++i) {
+    std::cin >> arr[i];
+  }
+
+  std::sort(arr + 1, arr + n + 1);
+
+  int ans = 0;
+  for (int i = 1; i <= n; ++i) {
+    ans += abs(arr[i] - arr[(n + 1) / 2]);
+  }
+
+  std::cout << ans << "\n";
+}
+
+signed main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+
+  int t = 1;
+  // std::cin >> t;
+
+  while (t--) {
+    solution();
+  }
+
+  return 0;
+}
